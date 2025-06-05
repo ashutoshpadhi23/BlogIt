cde1e198039640a1d9e1947dec17e08c544e6b2b
Ashutosh Padhi <ashutosh.padhi@bigbinary.com>
2025-06-05 08:24:29 +0000

    Added validations to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    MAXIMUM_TITLE_LENGTH = 125
+    MAXIMUM_DESCRIPTION_LENGTH =10000
+    validates :title, presence: true, length: {maximum: MAXIMUM_TITLE_LENGTH}
+    validates :description, presence: true, length: {maximum: MAXIMUM_DESCRIPTION_LENGTH}
+    validates_inclusion_of :is_bloggable, in: [true, false]
 end
