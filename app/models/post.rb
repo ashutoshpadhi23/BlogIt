0edbe575a8932984771a3b8062ee1e7e3cd1f907
ashutoshpadhi23 <ashutosh.padhi@bigbinary.com>
2025-06-06 15:05:34 +0000

    Added git hooks, eslint, prettier and rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
-    MAXIMUM_TITLE_LENGTH = 125
-    MAXIMUM_DESCRIPTION_LENGTH =10000
-    validates :title, presence: true, length: {maximum: MAXIMUM_TITLE_LENGTH}
-    validates :description, presence: true, length: {maximum: MAXIMUM_DESCRIPTION_LENGTH}
-    validates_inclusion_of :is_bloggable, in: [true, false]
+  MAXIMUM_TITLE_LENGTH = 125
+  MAXIMUM_DESCRIPTION_LENGTH = 10000
+  validates :title, presence: true, length: { maximum: MAXIMUM_TITLE_LENGTH }
+  validates :description, presence: true, length: { maximum: MAXIMUM_DESCRIPTION_LENGTH }
+  validates_inclusion_of :is_bloggable, in: [true, false]
 end
