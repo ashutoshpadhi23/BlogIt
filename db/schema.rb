6f78a1988752e33534e53adf03563c4b46fe9f48
Ashutosh Padhi <ashutosh.padhi@bigbinary.com>
2025-06-10 09:29:14 +0000

    Added slug to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_04_135400) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_10_092343) do
   create_table "posts", force: :cascade do |t|
     t.text "title", null: false
     t.text "description", null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_04_135400) do
     t.boolean "is_bloggable", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug", null: false
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
 end
