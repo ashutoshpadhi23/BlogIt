7ede7f135390d4b95321818b97701e005c8dc972
Ashutosh Padhi <ashutosh.padhi@bigbinary.com>
2025-06-12 13:04:27 +0000

    Added show and add post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,29 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  before_action :load_post!, only: %i[show]
   def index
     posts = Post.all
     render status: :ok, json: { posts: }
   end
+
+  def create
+    post = Post.new(post_params)
+    post.save!
+    render_notice("Post created successfully")
+  end
+
+  def show
+    render_json({ post: @post })
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :description)
+    end
+
+    def load_post!
+      @post = Post.find_by!(slug: params[:slug])
+    end
 end
---
+++ app/javascript/src/components/commons/index.js
@@ -0,0 +1,7 @@
+import Button from "./Button";
+import Input from "./Input";
+import PageLoader from "./PageLoader";
+import PageTitle from "./PageTitle";
+import Tooltip from "./Tooltip";
+
+export { Button, Input, PageLoader, PageTitle, Tooltip };
