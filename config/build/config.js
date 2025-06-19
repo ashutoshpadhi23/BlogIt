a72a13bcca8fbf3423ea624b4c1c147ee70fd755
bigbinary <bigbinary@bigbinarys-MacBook-Pro.local>
2025-06-19 11:05:45 +0530

    Setup neetoui

--- config/build/config.js
+++ config/build/config.js
@@ -2,10 +2,17 @@ import { createRequire } from "module";
 import path from "path";
 
 import { absolutePath } from "./constants.js";
+import { fileURLToPath } from 'url';
 
 const require = createRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const alias = {
+  "@bigbinary/neetoui/dist": path.resolve(
+    __dirname,
+    "../../node_modules/@bigbinary/neetoui/dist/index.css"
+  ),
   images: path.resolve(process.cwd(), "app/assets/images"),
   crypto: require.resolve("crypto-browserify"),
   path: require.resolve("path-browserify"),
@@ -15,9 +22,10 @@ const alias = {
   common: absolutePath("src/common"),
   components: absolutePath("src/components"),
   constants: absolutePath("src/constants"),
+  neetoui: "@bigbinary/neetoui",
+  neetoicons: "@bigbinary/neeto-icons",
   utils: absolutePath("src/utils"),
   assets: absolutePath("../assets"),
 };
 
 export { alias };
-
